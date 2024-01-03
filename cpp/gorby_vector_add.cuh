d4c09c4f42e952348b7fa7a0be7a4288aaa55f20
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2024-01-02 19:38:07 -0500

    Ran source files through formatter

--- cpp/gorby_vector_add.cuh
+++ cpp/gorby_vector_add.cuh
@@ -1,9 +1,10 @@
 #include <torch/extension.h>
 
 // Definitions
-torch::Tensor gorby_vector_add_1d_inplace( torch::Tensor x, torch::Tensor y );
+torch::Tensor gorby_vector_add_1d_inplace(torch::Tensor x, torch::Tensor y);
 
 // PyBind!
-PYBIND11_MODULE( TORCH_EXTENSION_NAME, m ) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
   m.def("gorby_vector_add_1d_inplace", &gorby_vector_add_1d_inplace, "CUDA 1D Vector Add, Inplace");
 }
--- main_cuda.py
+++ main_cuda.py
@@ -5,21 +5,21 @@ from gorby_vector_add_cuda import gorby_vector_add_1d_inplace
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l","--length", type=int, default=4)
+    parser.add_argument("-l", "--length", type=int, default=4)
     parser.add_argument("-d", "--device", type=str, default="cuda")
     args = parser.parse_args()
 
     x = torch.ones(size=(args.length,), dtype=torch.float32, device=args.device)
     y = torch.ones(size=(args.length,), dtype=torch.float32, device=args.device)
-    
-    if (args.length >= 4):
+
+    if args.length >= 4:
         print(f"x pre: {x[0:2]}...{x[-2:None]}\ny: {y[0:2]}...{y[-2:None]}")
     else:
         print(f"x pre: {x}\ny: {y}")
 
     gorby_vector_add_1d_inplace(x, y)
 
-    if (args.length >= 4):
+    if args.length >= 4:
         print(f"x post: {x[0:2]}...{x[-2:None]}\ny: {y[0:2]}...{y[-2:None]}")
     else:
         print(f"x post: {x}\ny: {y}")
